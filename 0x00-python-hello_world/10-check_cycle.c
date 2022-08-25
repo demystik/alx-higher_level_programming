b088c5319196188c3adf79afd00b884ae7bb61db
demystik <demystiktech@gmail.com>
2022-08-25 10:30:04 +0100

    singly linked list cycle task pushed

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * check_cycle - This fucntion checks if there is cycle in
+ * singly linked list
+ * @list: Pointer to the head
+ *
+ * Return: 0 if there is no cycle and 1 if otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *check;
+	listint_t *h;
+
+	check = list;
+	h = list;
+	while (check != NULL)
+	{
+		check = check->next;
+		if (check == h)
+			return (1);
+	}
+	return (0);
+
+}
