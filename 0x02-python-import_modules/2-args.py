714546d0ceb8e89e7113f711e1499e6228693640
demystik <demystiktech@gmail.com>
2022-08-27 12:28:37 +0100

    adjusting task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """Print the number of and list of arguments."""
     import sys
-lenght = len(sys.argv) - 1
-if lenght == 0:
-    print("0 arguments.")
-elif lenght == 1:
-    print("1 argument:")
-else:
-    print("{}: arguments".format(lenght))
-for i in range(lenght):
-    print("{}: {}".format((i + 1), sys.argv[i + 1]))
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
