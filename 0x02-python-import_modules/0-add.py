061af0621f82efa43ab91a9dba799918886666f5
demystik <demystiktech@gmail.com>
2022-08-27 12:36:31 +0100

    task 1 adjusting

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "main__":
 
-from add_0 import add
+    from add_0 import add
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
