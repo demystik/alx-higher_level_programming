24bdd450b09f273cde2880b6a581d5a4b4f71b2c
demystik <demystiktech@gmail.com>
2022-08-27 12:57:01 +0100

    task 0 adjusting

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def add(a, b):
-
     """My addition function
 
-   Args:
-    a: first integer
-    b: second integer
+    Args:
+        a: first integer
+        b: second integer
 
     Returns:
-   The return value. a + b
-   """
-return (a + b)
+        The return value. a + b
+    """
+    return (a + b)
