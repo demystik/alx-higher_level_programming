22c18f2af5cb5e0e24214075b9993d69c061f28f
demystik <demystiktech@gmail.com>
2022-08-30 18:58:47 +0100

    fixing

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    ta = ()
+    tb = ()
+    my_tup=()
+    ta = tuple_a + (0, 0)
+    tb = tuple_b + (0, 0)
+    my_tup = ta[0] + tb[0], ta[1] + tb[1]
+    return my_tup
