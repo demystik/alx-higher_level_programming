e413c79c044f191498806fab7e4e3c83ff377a28
demystik <demystiktech@gmail.com>
2022-08-30 10:07:18 +0100

    hot fix

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,9 +4,8 @@ def replace_in_list(my_list, idx, element):
 
     if idx < 0:
         return None
-    for i in my_list:
-        pass
-    if idx > (i - 1):
+    elif idx > len(my_list) - 1:
         return None
-    my_list[idx] = element
-    return my_list
+    else:
+        my_list[idx] = element
+        return my_list
