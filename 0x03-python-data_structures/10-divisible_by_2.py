462281e09c9da914e05b8596a1e365a8748da075
demystik <demystiktech@gmail.com>
2022-08-30 18:09:34 +0100

    fixing

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
-    if len(my_list)) == 0:
+    if len(my_list) == 0:
         return "None"
     for i in range(len(my_list)):
         pass
