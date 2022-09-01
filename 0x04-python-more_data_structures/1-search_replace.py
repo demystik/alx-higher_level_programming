9ccdca542909ea3671fe949e81a58d3ea47f20b2
demystik <demystiktech@gmail.com>
2022-09-01 04:40:09 +0100

    adjusting task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     for i in new_list:
         if i == search:
-            new_list[search] = replace
+            new_list[i - 1] = replace
     return new_list
     """
     new_list[search - 1] = replace
