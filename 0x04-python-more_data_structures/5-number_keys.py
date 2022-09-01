c6e87d864d862204e659c67974e7935fe848e406
demystik <demystiktech@gmail.com>
2022-09-01 04:23:41 +0100

    adjust task 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
+    get = 0
+    for i in list(a_dictionary.keys()):
+        get += 1
+    return get
+    """
     for i in range(len(a_dictionary)):
         pass
     return (i + 1)
+    """
