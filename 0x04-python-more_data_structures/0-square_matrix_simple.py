07e48b3fb4927d2bc9316d763b4c5ca98e8bd96a
demystik <demystiktech@gmail.com>
2022-09-01 04:47:43 +0100

    hot fixing

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
+def pow(x):
+    return x**2
 def square_matrix_simple(matrix=[]):
-    mat = matrix.copy()
-    def pow(x):
-        return x**2
-    for i in range(len(mat)):
-        mat[i] = list(map(pow, matrix[i]))
-    return mat
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(pow, matrix[i]))
+
+    return (new_matrix)
