fb5bf8e755895e0f88e5da0cce55f3dcb456175a
demystik <demystiktech@gmail.com>
2022-09-13 16:41:37 +0100

    adding some exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """Print x element of a list
+
+    Args:
+        my_list: The list to print from
+        x: number of element to print
+
+    Returns:
+        The number to element printed
+    """
+    num = 0
+    for element in range(x):
+        try:
+            print("{}".format(my_list[element]), end="")
+            num += 1
+        except IndexError:
+            break
+    print("")
+    return (num)
+
+
