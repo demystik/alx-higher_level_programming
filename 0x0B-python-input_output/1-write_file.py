6325e6679842e8a68e364f42aedf48376a9bed58
demystik <demystiktech@gmail.com>
2022-09-29 15:01:00 +0100

    task 1 added

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This function writes a string to a text file
+    and return number of characters written
+"""
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, 'w', encoding="utf-8") as file:
+        return file.write(text)
