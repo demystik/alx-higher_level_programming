5077e5ef091e6d09013e78a3127cad85e90abdc7
demystik <demystiktech@gmail.com>
2022-09-30 17:24:21 +0100

    task 7 updated

---
+++ python_restart/if_else/7-islower.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 123:
+        return True
+    return False
+
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
