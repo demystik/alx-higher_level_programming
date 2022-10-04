c3f61eb05645b779166a9dce86848c005c064638
demystik <demystiktech@gmail.com>
2022-10-04 20:34:28 +0100

    task 8 pushed

---
+++ python_restart/data_struct/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    first_char = sentence[0]
+    lenght = len(sentence)
+    ret_tuple = ()
+    ret_tuple = (lenght, first_char)
+    return (ret_tuple)
