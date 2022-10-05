727b1519c306a32d69662b352f09891a514d111a
demystik <demystiktech@gmail.com>
2022-10-05 04:24:47 +0100

    add int updated

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defines an integer addition function."""
+
+
 def add_integer(a, b=98):
     """Return the integer addition of a and b.
     Float arguments are typecasted to ints before addition is performed.
