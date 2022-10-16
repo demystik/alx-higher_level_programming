05e7b00d154069637f1847064e6917e48ae9ffa7
demystik <demystiktech@gmail.com>
2022-10-16 21:32:23 +0100

    task-2-added

---
+++ python_restart/class_and_object/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    """This class defines a square and initialize it"""
+    def __init__(self, size = 0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
