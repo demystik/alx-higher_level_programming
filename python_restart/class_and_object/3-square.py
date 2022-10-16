5c4ba76b27aaa4e804c72282c48fda94ff15dd1a
demystik <demystiktech@gmail.com>
2022-10-16 21:33:11 +0100

    task 3 gangan added

---
+++ python_restart/class_and_object/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class Square:
+    """This class Square that defines a square and inst method"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+    
