abe6d843afb028f957f5cc2ae83fad63564e57e6
demystik <demystiktech@gmail.com>
2023-03-13 22:05:10 +0100

    hot fixing

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const langs = ['C is fun', 'Python is cool', 'javaScript is amazing'];
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < langs.length; i++) {
   console.log(langs[i]);
 }
