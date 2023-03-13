dc611daacd32d4b38873044454d79a04dbebca75
demystik <demystiktech@gmail.com>
2023-03-13 21:49:53 +0100

    hot fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const process = require('process');
 
 const args = process.argv;
-if (args.length < 3) {
+if (!(args[2])) {
   console.log('No argument');
 } else {
   console.log('Argument found');
