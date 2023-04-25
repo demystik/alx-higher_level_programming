a78ba60ab91ded619d4432e16a6d716cdcadae3f
demystik <demystiktech@gmail.com>
2023-04-25 14:21:22 +0100

    5-htbn_header.py

---
+++ 0x11-python-network_1/5-htbn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL 
+and displays the value of the variable X-Request-Id 
+in the response header
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    resp = request.get(url)
+    print(resp.headers.get("X-Request-Id"))
