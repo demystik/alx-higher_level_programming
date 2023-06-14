a13151e881d88157da43f66ee0a436288db546d2
demystik <demystiktech@gmail.com>
2023-06-14 17:34:13 +0100

    task 1 added

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'Javascript is amazing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
