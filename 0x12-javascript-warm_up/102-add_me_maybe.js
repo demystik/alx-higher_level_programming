6f217a25af1d33b9c00191e4fe047cf95b0d35cd
demystik <demystiktech@gmail.com>
2023-06-14 17:57:34 +0100

    task 102 adjusted

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-exports.addMeMaybe = function(number, theFunction)
-{
-	number = number + 1;
-	theFunction(number);
-}
+exports.addMeMaybe = function (number, theFunction) {
+  number = number + 1;
+  theFunction(number);
+};
