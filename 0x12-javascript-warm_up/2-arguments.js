74f2eb62780d251d3558b4a85474983994dc7776
demystik <demystiktech@gmail.com>
2023-06-14 17:44:51 +0100

    task 2 adjusted

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (args.length < 0) { console.log('No argument'); } else if (args.lenght == 1) { console.log('Argument found'); } else { console.log('Arguments found'); }
+if (args.length < 0) { console.log('No argument'); } else if (args.lenght === 1) { console.log('Argument found'); } else { console.log('Arguments found'); }
