c53950c55435f3a4866e010c753a6b1fb9509ee1
demystik <demystiktech@gmail.com>
2023-06-16 16:00:35 +0100

    task 4 added

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i += 1) {
+      let rec = '';
+      for (let j = 0; j < this.width; j += 1) { rec += 'X'; }
+      console.log(rec);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
