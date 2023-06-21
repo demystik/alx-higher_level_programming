093291765429041c1a2babfda681a78faa6461be
demystik <demystiktech@gmail.com>
2023-06-21 08:18:48 +0100

    task 1 .py pushed

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY
+                   states.id")
+
+    result = cursor.fetchall()
+
+    for line in result:
+        print(line)
+
+    cursor.close()
+    db.close()
