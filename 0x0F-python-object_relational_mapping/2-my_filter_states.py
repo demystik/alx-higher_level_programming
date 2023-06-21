ba7fcde11848841d4742187e8e40dc604d6c025d
demystik <demystiktech@gmail.com>
2023-06-21 18:54:21 +0100

    task 2 added

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE
-                   BINARY '{}'".format(sys.argv[4]))
+    cursor.execute("""SELECT * FROM states WHERE name LIKE
+                   BINARY '{}'""".format(sys.argv[4]))
 
     result = cursor.fetchall()
 
