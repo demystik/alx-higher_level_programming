c0c4d607da4f90d8379e3e9d9afda3d890149422
demystik <demystiktech@gmail.com>
2023-07-01 22:04:32 +0100

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, 
+sends a request to the URL and displays
+the body of the response."""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    response = requests.get(sys.argv[1])
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
