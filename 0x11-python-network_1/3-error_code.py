f30afe5a5b491a1a2ed94b42f3eb62fd2f5e2e5e
demystik <demystiktech@gmail.com>
2023-07-01 20:55:33 +0100

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,11 +2,11 @@
 """ Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8)."""
 
-import sys
-from urllib import request, error
-
 if __name__ == "__main__":
+    import sys
+    from urllib import request, error
     url = sys.argv[1]
+
     try:
         with request.urlopen(url, timeout=10) as response:
             body = response.read.decode('utf-8')
