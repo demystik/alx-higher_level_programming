aea2b69425d7cedafac96e394c702d098e53fa6c
demystik <demystiktech@gmail.com>
2023-07-01 21:39:59 +0100

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response."""
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {"email": sys.argv[2]}
+
+    response = requests.post(url, email)
+    print(response)
