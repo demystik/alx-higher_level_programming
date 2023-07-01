7696c7baad3a6273df787bc9a58960b43084e9e9
demystik <demystiktech@gmail.com>
2023-07-01 21:45:15 +0100

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,6 +4,9 @@ sends a POST request to the passed URL with the email as a parameter,
 and finally displays the body of the response."""
 
 if __name__ == "__main__":
+    import sys
+    import requests
+
     url = sys.argv[1]
     email = {"email": sys.argv[2]}
 
