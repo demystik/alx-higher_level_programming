145c645c248215ef24072f23259ea0a59798ac83
demystik <demystiktech@gmail.com>
2023-07-01 08:14:42 +0100

    task 6 added

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 
 
 def find_peak(list_of_integers):
+    """This function finds a peak of list"""
     if (list_of_integers):
         list_of_integers.sort(reverse=True)
         return (list_of_integers[0])
