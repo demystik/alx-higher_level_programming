31174344335ccc7a8f48515c9f927715f5762559
demystik <demystiktech@gmail.com>
2023-07-01 08:10:50 +0100

    task 6 added

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This file does this and that"""
+
+
 def find_peak(list_of_integers):
-	if (list_of_integers):
-		list_of_integers.sort(reverse=True)
-		return(list_of_integers[0])
-	return None
+    if (list_of_integers):
+        list_of_integers.sort(reverse=True)
+        return (list_of_integers[0])
+    return None
