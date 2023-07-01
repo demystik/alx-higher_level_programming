f8557338c8b5f61556f57c2cc0e65f1edea14d82
demystik <demystiktech@gmail.com>
2023-07-01 21:11:14 +0100

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
 
         content = parse.urlencode(email).encode("ascii")
 
-        request = urllib.request.Request(url, data)
+        request = urllib.request.Request(url, data, method="POST")
         with request.urlopen(request) as response:
             print(response.read().decode("utf-8"))
