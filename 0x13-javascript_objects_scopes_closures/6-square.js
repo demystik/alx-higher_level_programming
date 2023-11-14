72f310e05032d535ac99e50d486f2ab7873969e5
demystik <demystiktech@gmail.com>
2023-11-14 19:20:07 +0100

    task 6 added

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,15 +3,11 @@
 const supSquare = require('./5-square.js');
 
 class Square extends supSquare {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
     if (c === undefined) { c = 'X'; }
     let shape = '';
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
         shape += c;
       }
       console.log(shape);
