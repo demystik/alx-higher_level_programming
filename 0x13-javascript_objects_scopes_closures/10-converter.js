c8ef2f5790343eea40c1e349d0fe2d050ad8f732
demystik <demystiktech@gmail.com>
2023-11-14 19:31:22 +0100

    task 10 added

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+!#/usr/bin/node
 
 exports.converter = function (base) {
   // Define a recursive helper function
@@ -8,7 +8,7 @@ exports.converter = function (base) {
     } else {
       // Calculate the remainder
       const remainder = number % base;
-
+      
       // Recursive call with quotient and current result
       return convertRecursive(Math.floor(number / base), remainder.toString() + result);
     }
@@ -18,15 +18,16 @@ exports.converter = function (base) {
   return function (number) {
     // Check if the input is a valid positive integer
     if (!Number.isInteger(number) || number < 0) {
-      throw new Error('Input must be a non-negative integer.');
+      throw new Error("Input must be a non-negative integer.");
     }
 
     // Handle the special case when the input number is 0
     if (number === 0) {
-      return '0';
+      return "0";
     }
 
     // Call the recursive helper function with the initial result as an empty string
-    return convertRecursive(number, '');
+    return convertRecursive(number, "");
   };
 };
+
