ef75f6f739932ac7278a6308a3dcfd60c123c164
demystik <demystiktech@gmail.com>
2023-11-16 07:22:44 +0100

    first advanced task pushed

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,23 @@
+#!/usr/bin/ndoe
+const data = require('./101-data');
+
+// Original dictionary
+const originalDict = data.dict;
+
+// New dictionary to store occurrences and corresponding user ids
+const occurrencesDict = {};
+
+// Iterate over the original dictionary
+for (const userId in originalDict) {
+  const occurrences = originalDict[userId];
+
+  // Check if the occurrences value is already a key in the new dictionary
+  if (occurrencesDict[occurrences]) {
+    occurrencesDict[occurrences].push(userId);
+  } else {
+    occurrencesDict[occurrences] = [userId];
+  }
+}
+
+// Print the new dictionary
+console.log(occurrencesDict);
