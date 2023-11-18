fa93dff3989d3bca561e5cc30344cff897f0018d
demystik <demystiktech@gmail.com>
2023-11-18 10:38:21 +0100

    task 8 added

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,6 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
