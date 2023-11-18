2974a30de8df8124c63fe3455c875e0ab8d107ff
demystik <demystiktech@gmail.com>
2023-11-18 11:16:32 +0100

    task 14 added

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+"""
+print the state object with the name passed as argument from the database
 """
 import sys
 from model_state import Base, State
@@ -11,6 +12,7 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
