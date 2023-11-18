da5b7b2e6f44e317d21e9bd005c09afdd21f9325
demystik <demystiktech@gmail.com>
2023-11-18 10:09:58 +0100

    alchemy model state pushed

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,6 +11,9 @@ Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
+    """
+    Class with id and name attributes of each states.
+    """
     __tablename__ = "states"
     id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
