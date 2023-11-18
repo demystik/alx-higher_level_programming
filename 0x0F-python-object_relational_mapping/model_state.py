0922b6a3b4ddb695e96be587e16ee20f51879023
demystik <demystiktech@gmail.com>
2023-11-18 10:02:59 +0100

    alchemy model state pushed

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""model state from which create a table to be inherited from"""
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, MetaData
@@ -7,7 +8,8 @@ from sqlalchemy import Column, Integer, String, MetaData
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
+
 class State(Base):
     __tablename__ = "states"
-    id = Column(Integer,nullable=False, unique=True, primary_key=True)
+    id = Column(Integer, nullable=False, unique=True, primary_key=True)
     name = Column(String(128), nullable=False)
