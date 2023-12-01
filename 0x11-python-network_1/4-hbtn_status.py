371cbf706cac122f174a9d1483cc7a0ae24e82a4
demystik <demystiktech@gmail.com>
2023-12-01 16:25:00 +0100

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+
+    import requests
+
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print(f"\t - type: {type(response.text)}")
+    print(f"\t - content: {response.text}")
