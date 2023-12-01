a6c7d330105f609133fd1dea62895f8eeea87522
demystik <demystiktech@gmail.com>
2023-12-01 16:44:22 +0100

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL 
+"""Python script that takes in a URL, sends a request to the URL
 and displays the body of the response."""
 
 if __name__ == "__main__":
