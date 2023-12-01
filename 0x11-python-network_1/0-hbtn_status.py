25f460af7951d48da3e31e6c5983bc796ede20a1
demystik <demystiktech@gmail.com>
2023-12-01 12:14:39 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""
-Python script that fetcheshttps://alx-intranet.hbtn.io/status
-"""
-
+"""This script fetches https://alx-intranet.hbtn.io/status"""
 from urllib import request
-if __name__ == "__main__":
 
+
+if __name__ == "__main__":
     with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        resp = response.read()
+        body = response.read()
     print("Body response:")
-    print(f"\t - type: {type(resp)}")
-    print(f"\t - content: {resp}")
-    print(f"\t - utf8 content: {body.decode('utf-8')}")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
