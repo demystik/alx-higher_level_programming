b1dde8fcaa530727b301ec9d06f27c608a417c88
demystik <demystiktech@gmail.com>
2023-12-01 12:02:01 +0100

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL, sends a request to the URL
+and displays the body of the response."""
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+    url = sys.argv[1]
+
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('UTF-8'))
+    except error.HTTPError as error:
+        print("Error code:", error.code)
