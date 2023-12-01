8e88f1739bf498c089fd437e6f32c7d09b74caf0
demystik <demystiktech@gmail.com>
2023-12-01 16:47:41 +0100

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Python script that takes in a URL and an email address
-sends a POST request to the passed URL with the email as a 
+sends a POST request to the passed URL with the email as a
 parameter, and finally displays the body of the response."""
 
 if __name__ == "__main__":
