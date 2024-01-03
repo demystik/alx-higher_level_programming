cf9c50eadda3e7f8fd9e7ebd859f8d850877eb63
demystik <demystiktech@gmail.com>
2024-01-03 13:43:18 +0100

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
-const fs = require('node:fs');
+const fs = require('fs').promises;
 
 fs.writeFile(args[0], args[1], 'utf-8', (error) => {
   if (error) {
