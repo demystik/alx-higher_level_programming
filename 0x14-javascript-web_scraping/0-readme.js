39f0263dcfdf7adfb9c53aa68388ecd7cfddf2cb
demystik <demystiktech@gmail.com>
2024-01-03 13:53:38 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,13 @@
 const args = process.argv.slice(2);
 const fs = require('fs').promises;
 
-fs.readFile(args[0], 'utf-8', (error, data) => {
-  if (error) {
-    console.log(error);
-  } else {
+async function readFileAsync() {
+  try {
+    const data = await fs.readFile(args[0], 'utf-8');
     console.log(data);
+  } catch (error) {
+    console.log(error);
   }
-});
+}
+
+readFileAsync()
