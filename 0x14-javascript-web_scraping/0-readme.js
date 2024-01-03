d3b25fa6de7fc12aeb5c993d71fd8ca39c4fa7ac
demystik <demystiktech@gmail.com>
2024-01-03 13:44:16 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
-const fs = require('node:fs');
+const fs = require('fs').promises;
 
 fs.readFile(args[0], 'utf-8', (error, data) => {
   if (error) {
