17eb5d20226139db3b338a2e91bf62add6f8f64c
demystik <demystiktech@gmail.com>
2024-01-03 13:39:51 +0100

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -9,11 +9,11 @@ const endpoint = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request(endpoint, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else if (response.statusCode != 200) {
-    console.log(`Failed to retrieve Data ${response.statusCode}`)
+  } else if (response.statusCode !== 200) {
+    console.log(`Failed to retrieve Data ${response.statusCode}`);
     process.exit(1);
   } else {
     const movieData = JSON.parse(body);
-    console.log(movieData.title)
+    console.log(movieData.title);
   }
 });
